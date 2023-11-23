2457cf89b3381d2eb9c5d1d081c12c68335f7de4
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-11-23 09:23:49 +0000

    Add walking skeleton

---
+++ src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function SlugPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>This is the post content</h1>
+    </main>
+  );
+}
---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function BlogsPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>This is the blogs page</h1>
+    </main>
+  );
+}
