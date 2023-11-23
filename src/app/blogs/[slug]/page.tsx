88a9ce9c50ea7466bdb811fa9ff5ecd8271e2f16
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-11-23 17:00:54 +0000

    Update blogs page

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,8 +1,29 @@
 import React from "react";
-export default function SlugPage() {
+import { getPosts } from "../../../lib/posts";
+import { getPostsBySlug } from "../../../lib/posts";
+import { notFound } from "next/navigation";
+
+type blogPostParams = {
+  params: {
+    slug: string;
+  };
+};
+
+export function generateStaticParams() {
+  const posts = getPosts();
+  return posts.map((post) => {
+    return { slug: post.slug };
+  });
+}
+export default function SlugPage({ params }: blogPostParams) {
+  const post = getPostsBySlug(params.slug);
+  if (!post) {
+    notFound();
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>This is the post content</h1>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </main>
   );
 }
