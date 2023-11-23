03ee0eacc04602b75c71fb57377416618b9135b8
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-11-23 09:37:26 +0000

    Update blogs page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,6 +3,7 @@ export default function BlogsPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>This is the blogs page</h1>
+      <h2>Follow me for more updates!</h2>
     </main>
   );
 }
